4fa8abd9d15b7524176f5aa42c2b52477afe2d6a
gsengupta2810 <gsengupta2810@gmail.com>
2025-08-20 15:51:17 +0530

    Update ContactSection.tsx
    resolving errors

--- src/components/portfolio/ContactSection.tsx
+++ src/components/portfolio/ContactSection.tsx
@@ -126,10 +126,10 @@ const ContactSection = () => {
                   </CardDescription>
                 </CardHeader>
                 <CardContent>
-                  <Button className="w-full" size="lg">
-                    <a href="https://drive.google.com/uc?export=download&id=1KxHFTB4px91GYXNMim-AVN3tnmbpDKkb" 
+                  <a href="https://drive.google.com/uc?export=download&id=1KxHFTB4px91GYXNMim-AVN3tnmbpDKkb" 
                        target="_blank" 
                        rel="noopener noreferrer">
+                  <Button className="w-full" size="lg">
                     <Download className="w-5 h-5 mr-2" />
                     Download Resume (PDF)
                   </Button>
